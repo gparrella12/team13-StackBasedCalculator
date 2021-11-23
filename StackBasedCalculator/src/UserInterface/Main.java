e0fec155920801c556c31a261e3a888237c59253
Ernesto Mancusi <ermancusi@users.noreply.github.com>
2021-11-23 17:06:54 +0100

    change name from main to Main

---
+++ StackBasedCalculator/src/UserInterface/Main.java
@@ -0,0 +1,35 @@
+package UserInterface;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindowInterface.fxml"));
+        Scene scene = new Scene(loader.load());
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setTitle("Calculator");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/Graphics/icon.png")));
+        ((CalculatorController) loader.getController()).init(stage);
+        stage.show();
+    }
+
+    public void run() {
+        launch();
+    }
+
+    public static void main(String[] args) {
+        Main mainWindow = new Main();
+        mainWindow.run();
+    }
+}

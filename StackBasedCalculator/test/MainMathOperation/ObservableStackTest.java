9e6f8a4f90614cca253c1945eed29260180e641d
Giuseppe Parrella <94001472+gparrella12@users.noreply.github.com>
2021-11-23 17:21:24 +0100

    Test for ObservableStack added

--- StackBasedCalculator/test/MainMathOperation/ObservableStackTest.java
+++ StackBasedCalculator/test/MainMathOperation/ObservableStackTest.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
-
 package MainMathOperation;
 
 import java.util.NoSuchElementException;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 

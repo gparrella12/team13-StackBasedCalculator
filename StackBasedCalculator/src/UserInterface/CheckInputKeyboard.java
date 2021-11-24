0958ed2548e25006b2a9a53ba03ef5780d9a4bf0
Ernesto Mancusi <ermancusi@users.noreply.github.com>
2021-11-24 19:46:21 +0100

    now is possible to write a+-j instead of a+-1j and +-j instead of +-1j

--- StackBasedCalculator/src/UserInterface/CheckInputKeyboard.java
+++ StackBasedCalculator/src/UserInterface/CheckInputKeyboard.java
@@ -84,11 +84,16 @@ public class CheckInputKeyboard {
         // Ex: 5+j (5+1j)
         Pattern patternD = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)([-|+]+[0-9]*\\.*[0-9]*)[j$]+");
 
+        // Matches ONLY imaginary number when 1j is written as j.
+        // Ex: +j (+1j)   
+        Pattern patternE = Pattern.compile("([-]*[0-9]*\\.*[0-9]*)[j$]");
+
         Matcher matcherA = patternA.matcher(numberNoWhiteSpace);
         Matcher matcherB = patternB.matcher(numberNoWhiteSpace);
         Matcher matcherC = patternC.matcher(numberNoWhiteSpace);
         Matcher matcherD = patternD.matcher(numberNoWhiteSpace);
-
+        Matcher matcherE = patternE.matcher(numberNoWhiteSpace);
+        
         if (matcherA.find()) {
             real = Double.parseDouble(matcherA.group(1));
             imaginary = Double.parseDouble(matcherA.group(2));
@@ -100,7 +105,19 @@ public class CheckInputKeyboard {
             imaginary = Double.parseDouble(matcherC.group(1));
         } else if (matcherD.find()) {
             real = Double.parseDouble(matcherD.group(1));
-            imaginary = 1.0;
+            if (matcherD.group(2).toString().equals("-")) {
+                imaginary = -1.0;
+            } else {
+                imaginary = 1.0;
+            }
+
+        } else if (matcherE.find()) {
+            real = 0.0;
+            if (matcherE.group(1).toString().equals("-")) {
+                imaginary = -1.0;
+            } else {
+                imaginary = 1.0;
+            }
         }
         List<Double> l = new ArrayList<>();
         l.add(0, real);

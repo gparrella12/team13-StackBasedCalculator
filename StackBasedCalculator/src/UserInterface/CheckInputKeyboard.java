5662afa0619b4f216d46f2b368c56b2b0e46bbdf
Ernesto Mancusi <ermancusi@users.noreply.github.com>
2021-11-24 19:34:53 +0100

    now is possible to write a+j instead of a+1j

--- StackBasedCalculator/src/UserInterface/CheckInputKeyboard.java
+++ StackBasedCalculator/src/UserInterface/CheckInputKeyboard.java
@@ -12,10 +12,10 @@ import java.util.regex.Pattern;
  * @author Speranza
  */
 public class CheckInputKeyboard {
- 
+
     private String[] stackOperations = {"dup", "over", "clear", "drop", "swap", "sqrt"};
-    private String[] mathOperations = {"+", "-", "*", "/", "sqrt","+-"};
- 
+    private String[] mathOperations = {"+", "-", "*", "/", "sqrt", "+-"};
+
     /**
      * The function checks if the operation inserted in the physical keyboard is
      * supported by the Calculator.
@@ -23,61 +23,72 @@ public class CheckInputKeyboard {
      * @return true is the operation is written in a right way, false otherwise.
      */
     public String checkOperation(String operation) {
- 
-        for (String s : stackOperations)
-            if (s.equals(operation))
+
+        for (String s : stackOperations) {
+            if (s.equals(operation)) {
                 return s;
- 
-        for (String s : mathOperations)
-            if (s.equals(operation))
+            }
+        }
+
+        for (String s : mathOperations) {
+            if (s.equals(operation)) {
                 return s;
- 
-        return null;            
- 
- 
+            }
+        }
+
+        return null;
+
     }
- 
- 
+
     public boolean checkIfComplex(String s) {
- 
+
         String numberNoWhiteSpace = s.replaceAll("\\s", "");
         // Matches complex number with BOTH real AND imaginary parts.  
         // Ex: -3-2.0i
         Pattern patternA = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)([-|+]+[0-9]+\\.?[0-9]*)[j$]+");
- 
+
         // Matches ONLY real number.
         // Ex: 3.145
         Pattern patternB = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)$");
- 
+
         // Matches ONLY imaginary number.
         // Ex: -10i
         Pattern patternC = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)[j$]");
- 
+
+        // Matches complex number when 1j is written as j.
+        // Ex: 5+j (5+1j)
+        Pattern patternD = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)([-|+]+[0-9]*\\.*[0-9]*)[j$]+");
         return patternA.matcher(numberNoWhiteSpace).matches()
                 || patternB.matcher(numberNoWhiteSpace).matches()
-                || patternC.matcher(numberNoWhiteSpace).matches();
+                || patternC.matcher(numberNoWhiteSpace).matches()
+                || patternD.matcher(numberNoWhiteSpace).matches();
     }
- 
+
     public List<Double> parsingfComplex(String s) {
-        Double real = null , imaginary = null;
+        Double real = null, imaginary = null;
         String numberNoWhiteSpace = s.replaceAll("\\s", "");
- 
+
         // Matches complex number with BOTH real AND imaginary parts.  
-        // Ex: -3-2.0i
+        // Ex: -3-2.0j
         Pattern patternA = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)([-|+]+[0-9]+\\.?[0-9]*)[j$]+");
- 
+
         // Matches ONLY real number.
         // Ex: 3.145
         Pattern patternB = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)$");
- 
+
         // Matches ONLY imaginary number.
-        // Ex: -10i
+        // Ex: -10j
         Pattern patternC = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)[j$]");
- 
+
+        // Matches complex number when 1j is written as j.
+        // Ex: 5+j (5+1j)
+        Pattern patternD = Pattern.compile("([-]?[0-9]+\\.?[0-9]*)([-|+]+[0-9]*\\.*[0-9]*)[j$]+");
+
         Matcher matcherA = patternA.matcher(numberNoWhiteSpace);
         Matcher matcherB = patternB.matcher(numberNoWhiteSpace);
         Matcher matcherC = patternC.matcher(numberNoWhiteSpace);
- 
+        Matcher matcherD = patternD.matcher(numberNoWhiteSpace);
+
         if (matcherA.find()) {
             real = Double.parseDouble(matcherA.group(1));
             imaginary = Double.parseDouble(matcherA.group(2));
@@ -87,11 +98,14 @@ public class CheckInputKeyboard {
         } else if (matcherC.find()) {
             real = 0.0;
             imaginary = Double.parseDouble(matcherC.group(1));
+        } else if (matcherD.find()) {
+            real = Double.parseDouble(matcherD.group(1));
+            imaginary = 1.0;
         }
         List<Double> l = new ArrayList<>();
         l.add(0, real);
         l.add(1, imaginary);
         return l;
     }
- 
+
 }

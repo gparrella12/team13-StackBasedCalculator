e43a06f6d708b449507308e4be68d8fbe1f6f220
Speranza Ranieri <httpsperanza@users.noreply.github.com>
2021-11-26 18:59:33 +0100

    changed class utility.

---
+++ StackBasedCalculator/src/UserInterface/CheckOperations.java
@@ -0,0 +1,43 @@
+package UserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * CheckOperations class contains the utility methods to check all the
+ keyboard entries.
+ *
+ * @author Speranza
+ */
+public class CheckOperations {
+
+    private String[] stackOperations = {"dup", "over", "clear", "drop", "swap"};
+    private String[] mathOperations = {"+", "-", "*", "/", "sqrt", "+-"};
+
+    /**
+     * The function checks if the operation inserted in the physical keyboard is
+     * supported by the Calculator.
+     *
+     * @return the operation.
+     */
+    public String checkOperation(String operation) {
+
+        for (String s : stackOperations) {
+            if (s.equals(operation)) {
+                return s;
+            }
+        }
+
+        for (String s : mathOperations) {
+            if (s.equals(operation)) {
+                return s;
+            }
+        }
+
+        return null;
+
+    }
+
+}

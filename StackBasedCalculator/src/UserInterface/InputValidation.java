eb314c6464b430a436002cc855fb2012e58ac364
Speranza Ranieri <httpsperanza@users.noreply.github.com>
2021-11-27 10:18:03 +0100

    changed class name

---
+++ StackBasedCalculator/src/UserInterface/InputValidation.java
@@ -0,0 +1,38 @@
+package UserInterface;
+
+/**
+ * InputValidation class contains the utility methods to check all the
+ keyboard entries.
+ *
+ * @author Speranza
+ */
+public class InputValidation {
+
+    private String[] stackOperations = {"dup", "over", "clear", "drop", "swap"};
+    private String[] mathOperations = {"+", "-", "*", "/", "sqrt", "+-"};
+
+    /**
+     * The function checks if the operation inserted in the physical keyboard is
+     * supported by the Calculator.
+     *
+     * @return the operation.
+     */
+    public String checkOperation(String operation) {
+
+        for (String s : stackOperations) {
+            if (s.equals(operation)) {
+                return s;
+            }
+        }
+
+        for (String s : mathOperations) {
+            if (s.equals(operation)) {
+                return s;
+            }
+        }
+
+        return null;
+
+    }
+
+}

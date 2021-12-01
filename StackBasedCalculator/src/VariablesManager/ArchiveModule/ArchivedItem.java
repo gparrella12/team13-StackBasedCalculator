52a00861f3eeb2c3aac1471042e8d09a4057740e
S0nFra <80450457+S0nFra@users.noreply.github.com>
2021-12-01 19:26:13 +0100

    VariableStorage - aggiunto ArchiveModule

---
+++ StackBasedCalculator/src/VariablesManager/ArchiveModule/ArchivedItem.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VariablesManager.ArchiveModule;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author fsonnessa
+ * @param <E>
+ */
+public class ArchivedItem<E> {
+    private final E element;
+    private final LocalDateTime archivingDate;
+    
+    public ArchivedItem(E element){
+        this.element = element;
+        this.archivingDate = LocalDateTime.now();
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+    public LocalDateTime getArchivingDate() {
+        return archivingDate;
+    }    
+}

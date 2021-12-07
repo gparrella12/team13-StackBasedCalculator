10932fea308afc19bb2c42dbfa7fc1ea8f8d8181
S0nFra <80450457+S0nFra@users.noreply.github.com>
2021-12-07 19:13:17 +0100

    Package Test BasicOperation Non ancora totalmente convertito

---
+++ StackBasedCalculator/test/UserDefinedOperation/BasicOperations/SubtractionOperationTest.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package UserDefinedOperation.BasicOperations;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fsonnessa
+ */
+public class SubtractionOperationTest {
+    
+    public SubtractionOperationTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of execute method, of class SubtractionOperation.
+     */
+    @Test
+    public void testExecute() {
+        System.out.println("execute");
+        SubtractionOperation instance = null;
+        instance.execute();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}

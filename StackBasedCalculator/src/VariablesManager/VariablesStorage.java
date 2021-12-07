15dee954504a48fd3c0083cb8935f6946a2a7a33
Ernesto Mancusi <ermancusi@users.noreply.github.com>
2021-12-07 13:00:56 +0100

    add doc

--- StackBasedCalculator/src/VariablesManager/VariablesStorage.java
+++ StackBasedCalculator/src/VariablesManager/VariablesStorage.java
@@ -16,120 +16,130 @@ import org.apache.commons.math3.complex.Complex;
 
 /**
  *
- * The class provides an implementation of a Variable Storage for the Calculator.
- * 
+ * The class provides an implementation of a Variable Storage for the
+ * Calculator.
+ *
  * @author fsonnessa
  */
 public class VariablesStorage implements Archivable {
+
     private ObservableMap<String, Complex> variables;
-    
+
     /**
-     * Creates a VariablesStorage object containing all the variables 
-     * defined by the user.
-     * 
+     * Creates a VariablesStorage object containing all the variables defined by
+     * the user.
+     *
      */
-    public VariablesStorage(){
+    public VariablesStorage() {
         variables = FXCollections.observableHashMap();
     }
-    
+
     /**
-     * Checks the correctness of the given name:
-     * the variable name must be only a letter (characther from 'a' to 'z')
+     * Checks the correctness of the given name: the variable name must be only
+     * a letter (characther from 'a' to 'z')
+     *
      * @param key is the variable name
      * @return the key passed as argument, in lower case
-     * @throws IllegalArgumentException 
+     * @throws IllegalArgumentException
      */
-    private String checkVarName(String key) throws IllegalArgumentException{        
+    private String checkVarName(String key) throws IllegalArgumentException {
         key = key.toLowerCase();
-        if (!Character.isLetter(key.charAt(0)) || key.length() > 1)
+        if (!Character.isLetter(key.charAt(0)) || key.length() > 1) {
             throw new IllegalArgumentException("Character not allowed");
+        }
         return key;
     }
-    
+
     /**
      * Combines a value to variable, starting from the variable and the value
+     *
      * @param name variable name
      * @param value number to save
      * @throws IllegalArgumentException
      */
-    public void save(String name, Complex value) throws IllegalArgumentException{        
-        String key = checkVarName(name);        
+    public void save(String name, Complex value) throws IllegalArgumentException {
+        String key = checkVarName(name);
         variables.put(key, value);
     }
-    
+
     /**
      * Returns the value of a variable previously saved, starting from its name
+     *
      * @param name variable name
      * @return value stored
      * @throws NoSuchElementException if variable is not found
      * @throws IllegalArgumentException
      */
-    public Complex getVariableValue(String name) throws NoSuchElementException, IllegalArgumentException{
-        String key = checkVarName(name);        
-        Complex result = variables.get(key);        
-        if (result == null){
+    public Complex getVariableValue(String name) throws NoSuchElementException, IllegalArgumentException {
+        String key = checkVarName(name);
+        Complex result = variables.get(key);
+        if (result == null) {
             throw new NoSuchElementException("Variable not found");
         }
         return result;
     }
-    
+
     /**
      * Removes a stored variable
+     *
      * @param name variable name
      * @throws NoSuchElementException
-     * @throws IllegalArgumentException 
+     * @throws IllegalArgumentException
      */
-    public void removeVariable(String name) throws NoSuchElementException, IllegalArgumentException{
-        String key = checkVarName(name);             
-        if (variables.remove(key) == null){
+    public void removeVariable(String name) throws NoSuchElementException, IllegalArgumentException {
+        String key = checkVarName(name);
+        if (variables.remove(key) == null) {
             throw new NoSuchElementException("Variable not found");
         }
     }
-    
+
     /**
      * Clear all stored variables
      */
-    public void clear(){
+    public void clear() {
         variables.clear();
     }
-    
+
     /**
      * Sum passed <code>value</code> to a stored variable value
+     *
      * @param name variable name
      * @param value to sum
      * @throws NoSuchElementException
      */
-    public void addToVariable(String name, Complex value){
+    public void addToVariable(String name, Complex value) {
         String key = checkVarName(name);
         Complex toAdd = getVariableValue(key);
-        variables.put(key, toAdd.add(value)); 
+        variables.put(key, toAdd.add(value));
 
     }
-    
+
     /**
      * Subtracts stored variable value to a passed <code>value</code>
+     *
      * @param name variable name
      * @param value to subtract
      * @throws NoSuchElementException
      */
-    public void subFromVariable(String name, Complex value) throws NoSuchElementException{
+    public void subFromVariable(String name, Complex value) throws NoSuchElementException {
         String key = checkVarName(name);
         Complex toSub = getVariableValue(key);
         variables.put(key, toSub.subtract(value));
     }
-    
-    
+
     /**
      * Sets an observer to a TableView and its columns to see saved variables
+     *
      * @param table is the table in witch show the variable and the value
      * @param varNameColumn is the column associated to the variables
      * @param varValueColumn is the column associated to the value
-     * 
-     * @see https://stackoverflow.com/questions/37171820/populating-a-tableview-with-a-hashmap-that-will-update-when-hashmap-changes
+     *
+     * @see
+     * https://stackoverflow.com/questions/37171820/populating-a-tableview-with-a-hashmap-that-will-update-when-hashmap-changes
      */
     public void setObserver(TableView<String> table, TableColumn<String, String> varNameColumn, TableColumn<String, Complex> varValueColumn) {
         ObservableList<String> keys = FXCollections.observableArrayList();
-        
+
         variables.addListener((MapChangeListener.Change<? extends String, ? extends Complex> change) -> {
             boolean removed = change.wasRemoved();
             if (removed != change.wasAdded()) {
@@ -141,25 +151,25 @@ public class VariablesStorage implements Archivable {
                 }
             }
         });
-        
+
         table.setItems(keys);
         varNameColumn.setCellValueFactory(cd -> Bindings.createStringBinding(() -> cd.getValue()));
         varValueColumn.setCellValueFactory(cd -> Bindings.valueAt(variables, cd.getValue()));
         table.getColumns().setAll(varNameColumn, varValueColumn);
     }
-    
+
     /**
      * Stores current state of saved variables to allow its restore
      */
     @Override
-    public ArchiveItem toSave() {        
-       HashMap<String, Complex> toSave = new HashMap<>();
-       for (Map.Entry<String, Complex> entry : variables.entrySet()){
+    public ArchiveItem toSave() {
+        HashMap<String, Complex> toSave = new HashMap<>();
+        for (Map.Entry<String, Complex> entry : variables.entrySet()) {
             toSave.put(entry.getKey(), entry.getValue());
-        }        
+        }
         return new ArchiveItem(toSave);
     }
-    
+
     /**
      * Restores the last saved state of variables
      */
@@ -167,17 +177,25 @@ public class VariablesStorage implements Archivable {
     public void toRestore(ArchiveItem state) {
         HashMap<String, Complex> toRestore = (HashMap<String, Complex>) state.getElement();
         variables.clear();
-        for(Map.Entry<String, Complex> entry : toRestore.entrySet()){
+        for (Map.Entry<String, Complex> entry : toRestore.entrySet()) {
             variables.put(entry.getKey(), entry.getValue());
         }
     }
-    
-    public int getSize(){
-    return variables.size();    
+
+    /**
+     * Returns the number of elements stored in the map 
+     * @return map size
+     */
+    public int getSize() {
+        return variables.size();
     }
     
+    /**
+     * Returns the toString of the map
+     * @return string
+     */
     @Override
-    public String toString(){
+    public String toString() {
         return variables.toString();
     }
 }

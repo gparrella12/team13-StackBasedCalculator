012c97e1c69f2241498c4cd7d1494d7ebd20c1a9
Ernesto Mancusi <ermancusi@users.noreply.github.com>
2021-12-08 13:12:38 +0100

    -

--- StackBasedCalculator/test/UserInterface/InputValidationTest.java
+++ StackBasedCalculator/test/UserInterface/InputValidationTest.java
@@ -1,9 +1,13 @@
 package UserInterface;
 
+import Operations.OperationsEnum;
 import Stack.ObservableStack;
+import UserInterface.Parser.ParserEnum;
+import UserInterface.Parser.ParserFactory;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.complex.ComplexFormat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import org.junit.Before;
@@ -14,11 +18,11 @@ import org.junit.Test;
  * @author Ernesto
  */
 public class InputValidationTest {
-    
+
     private ObservableStack<Complex> stack;
-    
+
     @Before
-    public void setUp(){
+    public void setUp() {
         stack = new ObservableStack<>();
     }
 
@@ -29,16 +33,16 @@ public class InputValidationTest {
      */
     @Test
     public void testParseComplex() {
-        InputValidation i = new InputValidation();
+        ParserFactory parser = new ParserFactory();
         System.out.println("\naddNum");
 
-        Scanner sc = new Scanner(new InputStreamReader(InputValidation.class.getResourceAsStream("TestCasesParser.csv")));
+        Scanner sc = new Scanner(new InputStreamReader(InputValidationTest.class.getResourceAsStream("TestCasesParser.csv")));
         sc.nextLine();
         sc.useDelimiter(";");
 
         String input, testResult;
         boolean exceptionFlag = false;
-
+        Complex number;
         while (sc.hasNext()) {
             input = sc.next().replace("\n", "").replace("\r", "");
             testResult = sc.next().replace("\n", "").replace("\r", "");
@@ -46,19 +50,36 @@ public class InputValidationTest {
             System.out.println("input: " + input + "\tresult: " + testResult);
 
             if (testResult.equals("fail")) {
+
                 try {
-                    stack.push(i.parseComplex(input, "j"));
-                } catch (NumberFormatException ex) {
+                    number = new ComplexFormat().parse(parser.getParser(ParserEnum.COMPLEXNUMBER).check(input));
+                } catch (NullPointerException e) {
+                    number = null;
+                }
+                stack.push(number);
+                if (number == null) {
                     System.out.println(" >> Fail for " + input);
                     exceptionFlag = true;
                 }
+
                 if (!exceptionFlag) {
                     throw new RuntimeException(">> Attention! this input < " + input + " > not fail!");
                 }
                 exceptionFlag = false;
             } else {
-                stack.push(i.parseComplex(input, "j"));
+                try {
+                    number = new ComplexFormat().parse(parser.getParser(ParserEnum.COMPLEXNUMBER).check(input));
+                } catch (NullPointerException e) {
+                    number = null;
+                }
+
+                if (number == null) {
+                    throw new RuntimeException(">> Attention! this input < " + input + " > not parsed!");
+                }
+
+                stack.push(number);
                 Complex tmp = stack.top();
+
                 assertEquals("Wrong parsing detect : in< " + input + " > out< " + testResult + " >] ", tmp.toString(), testResult);
                 stack.pop();
                 System.out.println(" >> OK");
@@ -74,22 +95,36 @@ public class InputValidationTest {
      */
     @Test
     public void testCheckOperation() {
-        InputValidation i = new InputValidation();
-        String[] stackOperations = {"dup", "over", "clear", "drop", "swap"};
-        String[] mathOperations = {"+", "-", "*", "/", "sqrt", "+-"};
+        ParserFactory parser = new ParserFactory();
+        OperationsEnum operation;
+        for (OperationsEnum e : OperationsEnum.values()) {
+            try {
+                operation = OperationsEnum.valueOfString(parser.getParser(ParserEnum.OPERATION).check(e.toString()));
+            } catch (UnsupportedOperationException ex) {
+                operation = null;
+            }
+            if (!e.equals(e.PUSH)) {
+                assertEquals("The inserted operation is invalid", e, operation);
+            }
 
-        for (String m : mathOperations) {
-            assertEquals("The inserted operation is invalid", m, i.checkOperation(m));
-        }
-        for (String s : stackOperations) {
-            assertEquals("The inserted operation is invalid", s, i.checkOperation(s));
         }
 
-        for (int k = 0; k < stackOperations.length; k++) {
-            for (int j = 0; j < mathOperations.length; j++) {
-                assertNotEquals("The inserted operation is invalid", mathOperations[j], i.checkOperation(stackOperations[k]));
+        OperationsEnum[] o = OperationsEnum.values();
+        for (int k = 0; k < o.length; k++) {
+            for (int j = o.length - 1; j > -1; j--) {
+                try {
+                    operation = OperationsEnum.valueOfString(parser.getParser(ParserEnum.OPERATION).check(o[j].toString()));
+                } catch (UnsupportedOperationException ex) {
+                    operation = null;
+                }
+
+                if (o[j] != o[k] && !o[j].equals(OperationsEnum.PUSH)) {
+                    assertNotEquals("The inserted operation is invalid", o[k], operation);
+                }
+
             }
         }
 
     }
+
 }
